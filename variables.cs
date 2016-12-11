b42900ba221f79752283cc542e199b547fb65fed
Deez <cactus@fixmonordi.com>
2016-12-11 15:43:42 -0500

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "deez";
+            bool isWorking = true;
+
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
