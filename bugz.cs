3f4d456c7187d7a35a0a78c1df987fc911e28540
Deez <cactus@fixmonordi.com>
2016-12-11 20:25:07 -0500

    Create bugz.cs

---
+++ bugz.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Bugz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bugs = (4 + 7);
+            var ants = (71 * 2);
+
+            Console.WriteLine("This is how many bugs there are in your basement");
+            Console.WriteLine(bugs);
+            Console.WriteLine("***************");
+            Console.WriteLine("***************");
+            Console.WriteLine("This is how many ants there are in your shoes");
+            Console.WriteLine(ants);
+            Console.WriteLine("***************");
+            Console.WriteLine("***************");
+            Console.WriteLine("This is how many shits there are in total");
+            Console.WriteLine(bugs + ants);
+
+        }
+    }
+}
