5fe3f8ce09884c4294b9c22a2edaa3c6e143abb0
Deez <cactus@fixmonordi.com>
2016-12-13 20:58:55 -0500

    Create Assembly.cs

---
+++ Assembly.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("No. One")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Learning")]
+[assembly: AssemblyCopyright("Copyright FMO ©2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("decfb2bb-af79-4e66-8f5d-ab2e94375e08")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
