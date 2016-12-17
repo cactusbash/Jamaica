69e0125b614bb9030fb0dcf971ca6a583bb2b5c5
Deez <cactus@fixmonordi.com>
2016-12-16 22:01:02 -0500

    Add files via upload

---
+++ Python/terms.py
@@ -0,0 +1,5 @@
+Variables				=	which store values for later use
+Data types				= such as numbers and booleans
+Whitespace				= which separates statements
+Comments				= which make your code easier to read
+Arithmetic operations	= including +, -, *, /, **, and %
