9ad2413ecbfb31662136db7e29b59ee8abd00f49
Deez <cactus@fixmonordi.com>
2016-12-16 22:02:15 -0500

    Delete terms.py

--- Python/terms.py
+++
@@ -1,5 +0,0 @@
-Variables				=	which store values for later use
-Data types				= such as numbers and booleans
-Whitespace				= which separates statements
-Comments				= which make your code easier to read
-Arithmetic operations	= including +, -, *, /, **, and %
